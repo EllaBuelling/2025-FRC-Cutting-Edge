f1bf24739446590d04ff6211ff1116c784a8c9c8
EdgewoodHsRobotics <edgewoodhighschoolrobotics@gmail.com>
2025-06-04 18:36:14 -0500

    test

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -16,27 +16,24 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
-public class Robot extends TimedRobot {
+public class Elevator extends TimedRobot {
   private final DifferentialDrive m_robotDrive;
-  private final Joystick m_leftStick;0;
-  private final Joystick m_rightStick;
+  private final Joystick triggerJoystick;0;
 
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  private final PWMSparkMax elevatorRightMotor = new PWMSparkMax(9);
 
   /** Called once at the beginning of the robot program. */
   public Robot() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    elevatorRightMotor.setInverted(true);
 
     m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    triggerJoystick = new Joystick(9);
 
     SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+
   }
 
   @Override
